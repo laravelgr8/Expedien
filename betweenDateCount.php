d53a81c1064a89afbe6bb8f35b1bdb4e668dad1b
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2021-10-06 11:58:33 +0530

    Update betweenDateCount.php

--- betweenDateCount.php
+++ betweenDateCount.php
@@ -8,3 +8,22 @@ $("#txt_to").blur(function(){
     var days=(Date.parse(end) - Date.parse(start)) / 86400000;
     $("#txt_tot_prog_day").attr('value',days);
 });
+
+
+
+
+
+count days between two days:-
+$("#txt_to").blur(function(){
+	var start  =$("#txt_from").val();  //from 
+	var end  = $("#txt_to").val();  //to
+	var days=(Date.parse(end) - Date.parse(start)) / 86400000;
+	$("#txt_tot_prog_day").attr('value',days);  //jaha pe total day show karna ho
+});
+
+
+to date hamesa from date se jyada hona chahiye:-
+$("#txt_to").focus(function(){
+	var txt_from= $("#txt_from").val();  //from kar value recived
+	$("#txt_to").attr('min',txt_from);  //to me min ka attribute add liya
+});
